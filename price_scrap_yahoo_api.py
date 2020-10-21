9317ff944a036f91ad1c8d1178de01f2c5d9bc24
Liberty0 <85418david@gmail.com>
2020-10-21 13:33:03 +0800

    scrap from yahoo API (TW) (test script)

--- price_scrap_yahoo_api.py
+++ price_scrap_yahoo_api.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def price_scraper(Market='TW',Code='2330',Tick='d'):
+def price_scraper(Market='TW',Code='2330',Tick='5m'):
     
     import requests 
     from bs4 import BeautifulSoup
@@ -21,36 +21,45 @@ def price_scraper(Market='TW',Code='2330',Tick='d'):
     High = []
     Low = []
     
-    # findsoup = soup.findAll("ta")
-    fulltext = soup.get_text()
-    print(len(fulltext))
-    # print(findsoup)
-    # retrive date & prices data
-    # Dates_span = soup.find("table",{"width":"100%"}).findAll("tr",{"bgcolor":"#ffffff"})
-    # for Data_line in Dates_span:
-    #     Dates_ele = Data_line.findChildren("td")
-    #     Dates.append(datetime.strptime(Dates_ele[0].get_text(),"%H:%M"))
-        # print(datetime.strptime(Dates_ele[0].get_text(),"%H:%M"))
+    fulltext = soup.get_text() # datetime, open, high, low, close, variation
+    sectionsplit = fulltext.split('[')
+    timesplit = sectionsplit[1].split("{")
+    for i in range(0,len(timesplit)):
+        itemsplit = timesplit[i].split(':')
+        for j in range(0, len(itemsplit)):
+            valuesplit = itemsplit[j].split(',')
+            if j == 1:
+                Dates_str.append(valuesplit[0])
+                if Tick == 'd':
+                    Dates.append(datetime.strptime(valuesplit[0],"%Y%m%d"))
+                elif Tick == '5m':
+                    Dates.append(datetime.strptime(valuesplit[0],"%Y%m%d%H%M"))
+                
+            elif j == 5:
+                Closes.append(valuesplit[0])
+            elif j == 3:
+                High.append(valuesplit[0])
+            elif j == 4:
+                Low.append(valuesplit[0])  
+            # print(itemsplit[j].split(','))
     
-    # pull close prices
-    # Nums_td = soup.findAll("td",{"class":["high","low"]})
-    # for i in range(0, len(Nums_td)):
-    #     if (i % 5) == 3:
-    #         # transfer string to num
-    #         Closes.append(locale.atof(Nums_td[i].get_text()))
-    #     elif (i % 5) == 1:
-    #         High.append(locale.atof(Nums_td[i].get_text()))
-    #     elif (i % 5) == 2:
-    #         Low.append(locale.atof(Nums_td[i].get_text()))
-            
-    # fig, ax = matplotlib.pyplot.subplots()
-    # ax.plot(Dates,Closes)
-    # ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
+    # print(Dates)
+    
+    fig, ax = matplotlib.pyplot.subplots()
+    ax.plot(Dates,Closes)
+    if Tick == 'd':
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%b %d"))
+    elif Tick == '5m':
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
+    
+    fig2 = matplotlib.pyplot.figure()  
+    ax2 = fig2.add_subplot()
+    ax2.plot(Closes)
     
     # return Dates_str, Closes, High, Low
 
 if __name__ == "__main__":
-    result = price_scraper("TW","2330","d")
+    result = price_scraper()
     
     # print("length of dates:" + str(len(result[0])))
     # print("length of close prices:" + str(len(result[1])))
