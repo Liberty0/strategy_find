2abc0d7d6bc680e08d520d55e3b0d694f6004a31
Liberty0 <85418david@gmail.com>
2020-10-24 17:12:46 +0800

    investor basic shape
    unknown bug observed in invest result

---
+++ invest.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+def invest(Closes,RSI):
+    
+    inv_gauge = 0
+    inv_rate = 50 / 100
+    
+    ini_balance = 100000
+    Cash = [ini_balance] * min(len(Closes),len(RSI))
+    Inved_amount = [0] * len(Cash)
+    Inved_value = [0] * len(Cash)
+    Balance = [ini_balance] * len(Cash)
+    
+    for i in range(1,len(Cash)):
+        rsi_i = i + (len(RSI) - len(Cash))
+        if RSI[rsi_i] < 40:
+            rsigd = 1
+        elif RSI[rsi_i] > 80:
+            rsigd = -1
+        else:
+            rsigd = 0
+        
+        Cls_i = i + (len(Closes) - len(Cash))
+        gd = rsigd
+        if gd > inv_gauge:
+            buyamout = Cash[i-1] * inv_rate
+            Cash[i] = Cash[i-1] - buyamout
+            Inved_amount[i] = Inved_amount[i-1] + buyamout/Closes[Cls_i]
+        elif gd < inv_gauge:
+            sellaoumt = Inved_amount[i-1] * inv_rate
+            Cash[i] = Cash[i-1] + sellaoumt*Closes[Cls_i]
+            Inved_amount[i] = Inved_amount[i-1] - sellaoumt
+            
+        Inved_value[i] = Inved_amount[i] * Closes[Cls_i]
+        Balance[i] = Cash[i] + Inved_value[i]
+            
+    return Balance, Cash, Inved_value
--- main.py
+++ main.py
@@ -2,24 +2,22 @@
 
 import price_scraper as scr
 import price_analysis as ana
+import invest as inv
 import matplotlib
 import numpy as np
 
 ## Inputs
-Market = "US" # TW or US
-Code = "GOOG"
-Tick = "d" # [5m 10m 30m d w m]
-
+Market = "TW" # TW or US
+Code = "0056"
+Tick = "w" # [5m 10m 30m d w m]
 
 date_price = scr.price_scraper(Market, Code, Tick)
-
-
 Dates = date_price[0]
 Closes = date_price[1]
 High = date_price[2]
 Low = date_price[3]
 
-analysis = ana.analysis(Closes,High,Low)
+analysis = ana.analysis(Closes, High, Low)
 Changes = analysis[0]
 RSI = analysis[1]
 DIF = analysis[2]
@@ -29,6 +27,10 @@ pDI = analysis[5]
 mDI = analysis[6]
 ADX = analysis[7]
 
+Inv_result = inv.invest(Closes, RSI)
+Balance = Inv_result[0]
+Cash = Inv_result[1]
+Inved_value = Inv_result[2]
 
 fig1 = matplotlib.pyplot.figure()
 ax1 = fig1.add_subplot(211)
@@ -67,7 +69,6 @@ elif Tick=='5m' or Tick=='10m' or Tick=='30m':
     ax3.bar(np.linspace(1,len(HIS),len(HIS)),HIS,width=HISwidth)
     # ax3.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
 ax4 = fig2.add_subplot(212)
-
 if Tick=='d' or Tick=='w' or Tick=='m':
     ax4.plot(Dates[(len(Dates)-1-len(pDI)):(len(Dates)-1)],pDI,'-')
     ax4.plot(Dates[(len(Dates)-1-len(mDI)):(len(Dates)-1)],mDI,'-')
@@ -78,5 +79,11 @@ elif Tick=='5m' or Tick=='10m' or Tick=='30m':
     ax4.plot(mDI,'-')
     ax4.plot(ADX,'-')
     # ax4.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
+    
+fig3 = matplotlib.pyplot.figure()
+ax5 = fig3.add_subplot(111)
+ax5.plot(Dates[(len(Dates)-1-len(Balance)):(len(Dates)-1)],Balance,'-')
+# ax5.plot(Dates[(len(Dates)-1-len(Inved_value)):(len(Dates)-1)],Inved_value,'-')
+# ax5.plot(Dates[(len(Dates)-1-len(Cash)):(len(Dates)-1)],Cash,'-')
     
 # matplotlib.pyplot.plot_date(Dates,Closes)
