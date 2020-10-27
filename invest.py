16b431a869dcfc6aca8a12d08ee256361ebb8a0c
Liberty0 <85418david@gmail.com>
2020-10-28 02:00:25 +0800

    data length specification
    parameter "Length" decide the length of the data to simulate with.
    The end-date is fix to be the latest day.

---
+++ invest.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+def invest(Closes,analysis):
+    # Change = analysis[0]
+    RSI = analysis[1]
+    # DIF = analysis[2]
+    # MACD = analysis[3]
+    HIS = analysis[4]
+    pDI = analysis[5]
+    mDI = analysis[6]
+    ADX = analysis[7]
+    
+    rsi_weight = 5
+    macd_weight = 3
+    adx_weight = 3
+    inv_gauge = 4
+    inv_rate = 30 / 100
+    
+    ini_balance = 100000
+    Cash = [ini_balance] * min(len(Closes),len(RSI),len(HIS),len(ADX))
+    Inved_amount = [0] * len(Cash)
+    Inved_value = [0] * len(Cash)
+    Balance = [ini_balance] * len(Cash)
+    
+    for i in range(1,len(Cash)):
+        # RSI
+        rsi_i = i + (len(RSI) - len(Cash))
+        
+        if i == 1:
+            rsigd_1 = 0
+        if RSI[rsi_i] < 40:
+            rsigd = 1
+        elif RSI[rsi_i] > 80:
+            rsigd = -1
+        else:
+            rsigd = rsigd_1
+        rsigd_1 = rsigd * 0.8
+        
+        # MACD
+        macd_i = i + (len(HIS) - len(Cash))
+        
+        if i == 1:
+            macdgd_1 = 0
+        if (HIS[macd_i]>0) and (HIS[macd_i-1]<0):
+            macdgd = 1
+        elif (HIS[macd_i]<0) and (HIS[macd_i-1]>0):
+            macdgd = -1
+        else:
+            macdgd = macdgd_1 *0.8
+        macdgd_1 = macdgd
+        
+        # ADX
+        adx_i = i + (len(ADX) - len(Cash))
+        
+        if i == 1:
+            adxgd_1 = 0
+        if ADX[adx_i]>25:
+            if (pDI[adx_i]-mDI[adx_i]) > 0:
+                adxgd = 1
+            elif (pDI[adx_i]-mDI[adx_i]) < 0:
+                adxgd = -1
+        else:
+            adxgd = adxgd_1 * 0.8
+        adxgd_1 = adxgd
+        
+        # investor
+        Cls_i = i + (len(Closes) - len(Cash))
+        gd = rsigd*rsi_weight + macdgd*macd_weight + adxgd*adx_weight
+        
+        if gd > inv_gauge:
+            buyamout = Cash[i-1] * inv_rate
+            Cash[i] = Cash[i-1] - buyamout
+            Inved_amount[i] = Inved_amount[i-1] + buyamout/Closes[Cls_i]
+        elif gd < inv_gauge:
+            sellaoumt = Inved_amount[i-1] * inv_rate
+            Cash[i] = Cash[i-1] + sellaoumt*Closes[Cls_i]
+            Inved_amount[i] = Inved_amount[i-1] - sellaoumt
+        else:
+            Cash[i] = Cash[i-1]
+            Inved_amount[i] = Inved_amount[i-1]
+            
+        Inved_value[i] = Inved_amount[i] * Closes[Cls_i]
+        Balance[i] = Cash[i] + Inved_value[i]
+            
+    return Balance, Cash, Inved_value
--- main.py
+++ main.py
@@ -2,81 +2,21 @@
 
 import price_scraper as scr
 import price_analysis as ana
-import matplotlib
-import numpy as np
+import invest as inv
+import plotter
 
 ## Inputs
-Market = "US" # TW or US
-Code = "GOOG"
+Market = "TW" # TW or US
+Code = "0050"
 Tick = "d" # [5m 10m 30m d w m]
+Length = -1
 
-
-date_price = scr.price_scraper(Market, Code, Tick)
-
-
-Dates = date_price[0]
+date_price = scr.price_scraper(Market, Code, Tick, Length)
 Closes = date_price[1]
-High = date_price[2]
-Low = date_price[3]
 
-analysis = ana.analysis(Closes,High,Low)
-Changes = analysis[0]
-RSI = analysis[1]
-DIF = analysis[2]
-MACD = analysis[3]
-HIS = analysis[4]
-pDI = analysis[5]
-mDI = analysis[6]
-ADX = analysis[7]
+analysis = ana.analysis(date_price)
 
+Inv_result = inv.invest(Closes, analysis)
 
-fig1 = matplotlib.pyplot.figure()
-ax1 = fig1.add_subplot(211)
-if Tick=='d' or Tick=='w' or Tick=='m':
-    ax1.plot(Dates,Closes,'-')
-    ax1.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%b %d"))
-elif Tick=='5m' or Tick=='10m' or Tick=='30m':
-    ax1.plot(Closes,'-')
-    # ax1.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
-ax2 = fig1.add_subplot(212)
-if Tick=='d' or Tick=='w' or Tick=='m':
-    ax2.plot(Dates[(len(Dates)-1-len(RSI)):(len(Dates)-1)],RSI,'-')
-    ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%b %d"))
-elif Tick=='5m' or Tick=='10m' or Tick=='30m':
-    ax2.plot(RSI,'-')
-    # ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
+plotter.plotter(Tick,date_price,analysis,Inv_result)
 
-fig2 = matplotlib.pyplot.figure()
-ax3 = fig2.add_subplot(211)
-if Tick=='d' or Tick=='w' or Tick=='m':
-    ax3.plot(Dates[(len(Dates)-1-len(DIF)):(len(Dates)-1)],DIF,'-')
-    ax3.plot(Dates[(len(Dates)-1-len(MACD)):(len(Dates)-1)],MACD,'-')
-    if Tick == 'd':
-        HISwidth = .5
-    elif Tick == 'w':
-        HISwidth = 3
-    elif Tick == 'm':
-        HISwidth = 13
-    ax3.bar(Dates[(len(Dates)-1-len(HIS)):(len(Dates)-1)],HIS,width=HISwidth)
-    ax3.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%b %d"))
-elif Tick=='5m' or Tick=='10m' or Tick=='30m':
-    ax3.plot(DIF,'-')
-    ax3.plot(MACD,'-')
-    if Tick == '5m':
-        HISwidth = 0.5
-    ax3.bar(np.linspace(1,len(HIS),len(HIS)),HIS,width=HISwidth)
-    # ax3.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
-ax4 = fig2.add_subplot(212)
-
-if Tick=='d' or Tick=='w' or Tick=='m':
-    ax4.plot(Dates[(len(Dates)-1-len(pDI)):(len(Dates)-1)],pDI,'-')
-    ax4.plot(Dates[(len(Dates)-1-len(mDI)):(len(Dates)-1)],mDI,'-')
-    ax4.plot(Dates[(len(Dates)-1-len(ADX)):(len(Dates)-1)],ADX,'-')
-    ax4.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%b %d"))
-elif Tick=='5m' or Tick=='10m' or Tick=='30m':
-    ax4.plot(pDI,'-')
-    ax4.plot(mDI,'-')
-    ax4.plot(ADX,'-')
-    # ax4.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
-    
-# matplotlib.pyplot.plot_date(Dates,Closes)
